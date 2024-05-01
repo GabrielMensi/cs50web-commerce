f7b96a9a5954cab7d0d95c103886d1526adbd958
gabrielmensi <gabriel.mensi7@yahoo.com.ar>
2024-05-01 15:18:41 -0300

    add models category and listing

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,31 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+
+class listing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=256)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.URLField(blank=True)
+    active = models.BooleanField(default=True)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="category_listings"
+    )
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="user_listings"
+    )
+
+    def __str__(self):
+        return self.title
