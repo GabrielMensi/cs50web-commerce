c4512591d691c5dbaa44674db32ae57b944403e3
gabrielmensi <gabriel.mensi7@yahoo.com.ar>
2024-06-23 19:05:21 -0300

    Refactor URLs to follow snake_case convention and update view names

--- auctions/urls.py
+++ auctions/urls.py
@@ -9,15 +9,11 @@ urlpatterns = [
     path("register", views.register, name="register"),
     path("create", views.create_listing, name="create_listing"),
     path("listing/<int:id>", views.listing, name="listing"),
-    path("displayCategory", views.displayCategory, name="displayCategory"),
-    path(
-        "removeWatchlist/<int:id>",
-        views.removeWatchlist,
-        name="removeWatchlist"
-    ),
-    path("addWatchlist/<int:id>", views.addWatchlist, name="addWatchlist"),
+    path("category", views.display_category, name="display_category"),
+    path("remove_watchlist/<int:id>", views.remove_watchlist, name="remove_watchlist"),
+    path("add_watchlist/<int:id>", views.add_watchlist, name="add_watchlist"),
     path("watchlist", views.watchlist, name="watchlist"),
-    path("addComment/<int:id>", views.addComment, name="addComment"),
-    path("addBid/<int:id>", views.addBid, name="addBid"),
-    path("closeAuction/<int:id>", views.closeAuction, name="closeAuction")
+    path("add_comment/<int:id>", views.add_comment, name="add_comment"),
+    path("add_bid/<int:id>", views.add_bid, name="add_bid"),
+    path("close_auction/<int:id>", views.close_auction, name="close_auction"),
 ]
